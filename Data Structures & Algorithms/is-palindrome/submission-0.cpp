8fb6d86b7d8db2d5f8feb299445a58100df77661
Vaibhav Agrawal <vaibhav.bandhan.ai.2005@gmail.com>
2026-07-02 23:18:13 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        int st=0;
+        int en=s.length()-1;
+        while(st<en){
+            if(!isalnum(s[st])){
+                st++;
+            }
+            else if(!isalnum(s[en])){
+                en--;
+            }
+            else{
+            if(tolower(s[st])!=tolower(s[en])){
+                return false;
+            }
+            st++;
+            en--;
+            }
+        }
+        return true;
+
+    }
+};
